7e840d06dec53ad497849aa94b9e028dc421f663
midamaga22 <117557009+midamaga22@users.noreply.github.com>
2024-09-06 16:48:23 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+ <Suspense>
+  <template #default>
+    <component :is="HomeFlowComponent" />
+  </template>
+  <template #fallback>
+    <splashscreen/>
+  </template>
+ </Suspense>
+</template>
+
+<script setup>
+
+import splashscreen from './components/SplashScreen.vue';
+import { defineAsyncComponent } from 'vue';
+
+
+const HomeFlowComponent = defineAsyncComponent(()=>
+  new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve(import("./components/HomeFlow.vue"));
+    }, 2500);
+  }));
+
+
+
+
+</script>
+
+<style>
+Html,
+body,
+.app{
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green:#04b500;
+  --brand-blue:#0689b0;
+}
+</style>
---
+++ src/components/HomeFlow.vue
@@ -0,0 +1,115 @@
+<template>
+    <LayoutComponents>
+        <template #Header>
+            <HeaderComponents></HeaderComponents>
+        </template>
+        <template #Resume>
+            <Resume 
+            :total-label = "'Ahorro total'"
+            :label="label"
+            :total-Amount = "totalAmount"
+            :amount = "amount"
+        >
+            <template #graphic>
+                <Graphic :amounts="amounts" @select="select"/>
+            </template>
+            <template #action>
+                <Action @create = "create"/>
+            </template>
+        </Resume>
+        </template>
+        <template #Movements>
+        <Movements 
+        :movements ="movements" 
+        @remove="remove"
+        />
+        </template>
+    </LayoutComponents>
+  
+</template>
+
+<script >
+
+import LayoutComponents from '@/components/LayoutComponents.vue';
+import HeaderComponents from '@/components/HeaderComponents.vue';
+import Resume from '@/components/Resume/index.vue';
+import Action from './Action.vue';
+import Movements from '@/components/Movements/IndexUno.vue'
+import Graphic from './Resume/Graphic.vue';
+
+export default {
+    components: {
+        LayoutComponents,
+        HeaderComponents,
+        Resume,
+        Action,
+        Movements,
+        Graphic,
+
+    },
+    data() {
+        return {
+            label: null,
+            amount: null,
+            movements: [],
+        };
+    },
+    computed: {
+      amounts() {
+        const lastDays = this.movements
+        .filter(m =>{
+          const today = new Date();
+          const oldDate = today.setDate(today.getDate() -30);
+
+          return m.time > m.amount;
+        })
+        .map (m => m.amount);
+
+       return lastDays.map((m, i) => {
+        const lastMovements = lastDays.slice(0, i + 1);
+
+        return lastMovements.reduce((suma, movement) => {
+          return suma + movement
+        }, 0);
+       }); 
+      },
+      totalAmount() {
+      return this.movements.reduce((suma, m) => {
+        return suma + m.amount;
+      }, 0);
+    }
+  },
+      mounted(){
+        const movements = JSON.parse(localStorage.getItem("movements"));
+        console.log(movements);
+
+        if (Array.isArray(movements)){
+          this.movements = movements.map(m =>{
+          return {...m, time: new Date(m.time) };
+        });
+        }
+      
+      },
+    
+    methods: {
+      create(movement){
+        this.movements.push(movement);
+        this.save();
+      },
+      remove(id){
+       const index = this.movements.findIndex(m => m.id === id);
+       this.movements.splice(index, 1);
+       this.save();
+      },
+      save(){
+        localStorage.setItem("movements", JSON.stringify(this.movements));
+      },
+      select(el) {
+        console.log(el);
+        this.amount = el;
+      }
+    }
+};
+
+
+</script>
